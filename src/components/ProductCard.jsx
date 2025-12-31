0110e5794b6d8c0956be60010013a6087c7aecc1
Luca <lucacurto01@gmail.com>
2025-12-31 17:22:41 +0100

    Modifica css Card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,17 +3,27 @@ import './ProductCard.css';
 export default function ProductCard({ id, name, category, price, quantity, onDelete }) {
     return (
         <div className='container-card'>
-            <span className='item'>{name}</span>
-            <span className='item'>{category}</span>
-            <div className='dettaglio'>
-                <span>Prezzo: </span>
+
+            <div className='wrapper-drag-handle'>
+                <div className='drag-handle'>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+
+            <div className='container-dettagli'>
+                <span className='item'>{name}</span>
+                <span className='item'>{category}</span>
                 <span className='item'>{price}</span>
-            </div>
-            <div className='dettaglio'>
-                <span>Quantità: </span>
                 <span className='item'>{quantity}</span>
+
+                <button className='btn' onClick={() => { onDelete(id) }}>Rimuovi</button>
             </div>
-            <button className='btn' onClick={() => { onDelete(id) }}>Rimuovi</button>
+
         </div>
     );
 }
